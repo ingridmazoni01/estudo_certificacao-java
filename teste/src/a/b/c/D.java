91dd5d243ad406b8d2bbf4aa213689d631e39e2f
ingridmazoni01 <ingrid.mazoni01@fatec.sp.gov.br>
2018-08-22 18:05:07 -0300

    pacote a

---
+++ teste/src/a/b/c/D.java
@@ -0,0 +1,30 @@
+package a.b.c;
+
+import java.io.File;
+import java.util.*;
+
+public class D {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> existing =new ArrayList<String>();
+		
+		for(String arg:args) {
+			
+			if(new E().exists(arg))
+				existing.add(arg);
+		}
+		
+
+	}
+
+}
+
+class E{
+	
+	public boolean exists(String name) {
+		File f=new File(name);
+		
+		return f.exists();
+	}
+}
